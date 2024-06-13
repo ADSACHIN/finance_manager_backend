c23840ed9ca4ed3f16cb9bf0c94f2d58000d2292
sachin <127670514+ADSACHIN@users.noreply.github.com>
2024-06-13 22:24:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 // CORS Configuration
 app.use(cors({
-  origin: 'https://ADSACHIN.github.io/finance_manager/', // Replace with your frontend URL
+  origin: 'http://localhost:3000', // Replace with your frontend URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 }));
